a0d468d1a7d94c1381f669e7100c2e712731ea69
xueyongle <kratos199901@163.com>
2024-01-13 21:34:26 +0800

    third commit

--- src/main/java/com/xxxyl/git/GitTest.java
+++ src/main/java/com/xxxyl/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("asdasd");
         System.out.println("hot-fix");
         System.out.println("hot-fix2");
+        System.out.println("master 3");
     }
 }
