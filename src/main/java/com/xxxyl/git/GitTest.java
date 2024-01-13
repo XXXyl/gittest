9609ea7d0b0bfdef99c950138187efda11eff829
xueyongle <kratos199901@163.com>
2024-01-13 21:30:15 +0800

    hot-fix commit2

--- src/main/java/com/xxxyl/git/GitTest.java
+++ src/main/java/com/xxxyl/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("asdasd");
         System.out.println("hot-fix");
+        System.out.println("hot-fix2");
     }
 }
