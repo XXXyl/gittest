30c3d500499da58bad593e99419ea52e0754413b
xueyongle <kratos199901@163.com>
2024-01-13 21:20:09 +0800

    second commit

--- src/main/java/com/xxxyl/git/GitTest.java
+++ src/main/java/com/xxxyl/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("asdasd");
+        System.out.println("2tijiao");
     }
 }
